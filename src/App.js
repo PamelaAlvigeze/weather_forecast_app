8d47a59b5abce024b198ec6faac9ecc3f6fec6c9
PamelaAlvigeze <paavigeze@gmail.com>
2022-02-17 15:48:41 -0300

    cria projeto de previsao do tempo para a cidade de sao paulo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+  const apiBaseUrl  = 'https://api.hgbrasil.com/weather?woid=449704&format=json-cors&locale=pt';
+  const [ data, setData ] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(apiBaseUrl);
+        const data = await response.json();
+
+        setData(data);
+
+      } catch(e) {
+        return null;
+      }
+    }
+      fetchData();
+    }, []);
+
+    return (
+      <div className="container">
+        <h4> { data?.results?.city_name }</h4>
+
+        <table className="center-text__table table table-primary table-striped table-hover">
+          <thead>
+            <tr className='table-danger'>
+              <th> Data </th>
+              <th> Min. </th>
+              <th> Max. </th>
+              <th> Previsão </th>
+              <th>Icone</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              data?.results?.forecast.map((day, index) => {
+                return(
+                  <tr key={index}>
+                    <td>{day.date}</td>
+                    <td>{day.min}</td>
+                    <td>{day.max}</td>
+                    <td>{day.description}</td>
+                    <td><img src={`/weather-icons/${day.condition}.svg`} alt={day.description}/></td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+
+    // exemplo da chamada com axios
+    // axios.get(this.apiBaseUrl).then(
+    //   ({ data }) => {
+    //     this.setState({
+    //       city: data.results.city_name,
+    //       forecast: data.results.forecast,
+    //     })
+    // });
